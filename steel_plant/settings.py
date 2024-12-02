7daa23286dfaf28368928dfe1336ace5561ff057
Hari Veera Venkat Pasapuleti <haripasapuleti8055@gmail.com>
2024-12-02 17:01:38 +0530

    okay

--- steel_plant/settings.py
+++ steel_plant/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from . info import *
 
@@ -87,7 +88,8 @@ WSGI_APPLICATION = 'steel_plant.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'staticfiles', 'db.sqlite3'),
     }
 }
 
