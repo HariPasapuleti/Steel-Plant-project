f258c76c0474ea84c925ffb024eabb5709eec94a
Hari Veera Venkat Pasapuleti <haripasapuleti8055@gmail.com>
2024-12-04 17:07:32 +0530

    Deploying at render

--- steel_plant/settings.py
+++ steel_plant/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from . info import *
-import django_heroku
+# import django_heroku
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
